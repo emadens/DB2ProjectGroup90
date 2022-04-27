a993fe6f5d15a827e217288f32221116220095ab
ema <ema.baldelli02@gmail.com>
2022-04-27 16:52:43 +0200

    employee servlet

---
+++ src/main/java/it/polimi/example/db2test/Web/EmployeeLogin.java
@@ -0,0 +1,94 @@
+package it.polimi.example.db2test.Web;
+
+import it.polimi.example.db2test.EJB.Entities.Employee;
+import it.polimi.example.db2test.EJB.Exceptions.CredentialsException;
+import it.polimi.example.db2test.EJB.Services.EmployeeService;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.ejb.EJB;
+import javax.persistence.NonUniqueResultException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/EmployeeLogin")
+public class EmployeeLogin extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private TemplateEngine templateEngine;
+    @EJB(name = "EJB/com/example/db_test2/EmployeeService.java")
+    private EmployeeService eService;
+
+    public EmployeeLogin() {
+        super();
+    }
+
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String path = "/WEB-INF/employeeLoginForm.html";
+        ServletContext servletContext = getServletContext();
+        final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+        templateEngine.process(path, ctx, response.getWriter());
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name, password;
+
+        try {
+            name = StringEscapeUtils.escapeJava(request.getParameter("name"));
+            password = StringEscapeUtils.escapeJava(request.getParameter("password"));
+            if (name == null || password == null || name.isEmpty() || password.isEmpty()) {
+                throw new Exception("Missing or empty credential value");
+            }
+        } catch (Exception e) {
+            // for debugging only e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing credential value");
+            return;
+        }
+        Employee employee;
+        try {
+            // query db to authenticate for user
+            employee = eService.checkCredentials(name, password);
+        } catch (CredentialsException | NonUniqueResultException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Could not check credentials");
+            return;
+        }
+
+        // If the user exists, add info to the session and go to home page, otherwise
+        // show login page with error message
+
+        // deleted queryService - can give possible problems
+        String path;
+        if (employee == null) {
+            ServletContext servletContext = getServletContext();
+            final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+            ctx.setVariable("errorMsg", "Incorrect name or password");
+            path = "/WEB-INF/employeeLoginForm.html";
+            templateEngine.process(path, ctx, response.getWriter());
+        } else {
+            path = getServletContext().getContextPath() + "/EmployeeHome"; //tbd
+            response.sendRedirect(path);
+        }
+    }
+    public void destroy() {
+    }
+}
